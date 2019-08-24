e8d0aa55f898ca631fee742812426a7a822918ed
stesnyagin-s <forgonn@gmail.com>
2019-08-24 18:19:16 +0300

    init

---
+++ src/main/java/com/lrn/chat/ChatController.java
@@ -0,0 +1,100 @@
+package com.lrn.chat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("chat")
+public class ChatController {
+    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
+
+    private Queue<String> messages = new ConcurrentLinkedQueue<>();
+    private Map<String, String> usersOnline = new ConcurrentHashMap<>();
+
+    /**
+     * curl -X POST -i localhost:8080/chat/login -d "name=I_AM_STUPID"
+     */
+    @RequestMapping(
+            path = "login",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> login(@RequestParam("name") String name) {
+        if (name.length() < 1) {
+            return ResponseEntity.badRequest().body("Too short name, sorry :(");
+        }
+        if (name.length() > 20) {
+            return ResponseEntity.badRequest().body("Too long name, sorry :(");
+        }
+        if (usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("Already logged in:(");
+        }
+        usersOnline.put(name, name);
+        messages.add("[" + name + "] logged in");
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * curl -i localhost:8080/chat/chat
+     */
+    @RequestMapping(
+            path = "chat",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> chat() {
+        return new ResponseEntity<>(messages.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("\n")),
+                HttpStatus.OK);
+    }
+
+    /**
+     * curl -i localhost:8080/chat/online
+     */
+    @RequestMapping(
+            path = "online",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity online() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+    }
+
+    /**
+     * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
+     */
+    @RequestMapping(
+            path = "logout",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity logout(@RequestParam("name") String name) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+    }
+
+
+    /**
+     * curl -X POST -i localhost:8080/chat/say -d "name=I_AM_STUPID&msg=Hello everyone in this chat"
+     */
+    @RequestMapping(
+            path = "say",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+    }
+}
